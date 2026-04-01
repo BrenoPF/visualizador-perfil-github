bd7b094a7e06cd87b6d40ec5c63ed07df18783a5
Brenopf <brenol2008@hotmail.com>
2026-04-01 15:13:01 -0300

    feat: adiciona lista com 10 ultimos repositórios

--- JS/index.js
+++ JS/index.js
@@ -1,32 +1,33 @@
-import { getUser } from './services/api.js';
-import { renderLoading, renderUser, renderError, renderEmptyInput } from './ui/screen.js';
+import { fetchGithubUser, fetchGithubUserRepos } from './services/githubApi.js';
+import { renderProfile } from './ui/profileView.js';
 
 const inputSearch = document.getElementById('input-search');
 const btnSearch = document.getElementById('btn-search');
+const profileResults = document.querySelector('.profile-results');
 
-async function handleSearch() {
-    const userName = inputSearch.value.trim();
-
+async function getUserProfile() {
+    const userName = inputSearch.value;
     if (!userName) {
-        renderEmptyInput();
+        alert('Por favor, digite um nome de usuário do GitHub.');
+        profileResults.innerHTML = "";
         return;
     }
-
-    renderLoading();
-    
+    profileResults.innerHTML = `<p class="loading">Carregando...</p>`;
     try {
-        const userData = await getUser(userName);
-        renderUser(userData);
+        const userData = await fetchGithubUser(userName);
+        const userRepos = await fetchGithubUserRepos(userName);
+        renderProfile(userData, userRepos, profileResults);
     } catch (error) {
         console.error('Erro ao buscar o perfil do usuário:', error);
-        renderError('Usuário não encontrado. Por favor, verifique o nome de usuário e tente novamente.');
+        alert('Usuário não encontrado. Por favor, verifique o nome de usuário e tente novamente.');
+        profileResults.innerHTML = "";
     }
 }
 
-btnSearch.addEventListener('click', handleSearch);
+btnSearch.addEventListener('click', getUserProfile);
 
-inputSearch.addEventListener('keyup', (e) => {
-    if (e.key === 'Enter') {
-        handleSearch();
+inputSearch.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+        getUserProfile();
     }
 });
